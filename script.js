fe486e5d3a2c1dea51bfc88ba85b25768dc9edbf
Ryan <109833217+Ryany0@users.noreply.github.com>
2025-05-24 23:11:18 -0700

    Add book and addbooktolibrary function
    Book contstructor will create a new book with values and addbooktolibrary will add Book object to the library array.

--- script.js
+++ script.js
@@ -1,9 +1,17 @@
 const myLibrary = [];
 
-function Book(title, author) {
-    
+function Book(title, author, id) {
+    if (!new.target) {
+        throw Error("Use new to for constructor.");
+    }
+
+    this.title = title;
+    this.author = author;
+    this.id = id;
 }
 
-function addBookToLibrary() {
-
+function addBookToLibrary(title, author) {
+    const uid = self.crypto.randomUUID();
+    const book = new Book(title, author, uid);
+    myLibrary.push(book);
 }
